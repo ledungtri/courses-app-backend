7fec9325fa2e32d74015cca6c41906f44b33aeb1
Tri Bill Le <ledungtri.2202@gmail.com>
2021-11-01 19:35:25 -0600

    Implement searching people by attendingYear and instructingYear

--- src/graphql/queries/PeopleQuery.ts
+++ src/graphql/queries/PeopleQuery.ts
@@ -1,10 +1,24 @@
-import { GraphQLList } from "graphql";
+import { GraphQLInt, GraphQLList } from "graphql";
 import PersonType from "../types/PersonType";
 import Person from "../../db/models/Person";
 
 export default {
   type: new GraphQLList(PersonType),
-  resolve: () => {
-    return Person.find().sort({ "_private.sortParam": 1 });
+  args: {
+    attendingYear: {type: GraphQLInt},
+    instructingYear: {type: GraphQLInt}
+  },
+  resolve: (_: any, args: any) => {
+    const query: any = {};
+
+    if (args.attendingYear) {
+      query['attendances.year'] = args.attendingYear
+    }
+
+    if (args.instructingYear) {
+      query['instructions.year'] = args.instructingYear
+    }
+
+    return Person.find(query).sort({ "_private.sortParam": 1 });
   }
 };
